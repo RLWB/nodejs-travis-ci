ed3a3b8364b01051bf287dce44970a3ce4591767
huirui <huirui@newbeescm.com>
2018-08-07 12:10:43 +0800

    new

--- server.js
+++ server.js
@@ -12,4 +12,5 @@ const server = app.listen(8081, (res)=>{
     let port = server.address().port
     let host = ip.address();
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    process.exit();
 });
